d8ddd97a7a322937b81f6a872283e2fd20a51c2b
yongjin <korea9053@gmail.com>
2023-08-22 23:24:16 +0900

    commit test

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,8 +1,12 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.TestRequestBodyDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("test")
@@ -15,5 +19,39 @@ public class TestController {
     @GetMapping("/testGetMapping")
     public String testControllerWithPath() {
         return "Hello World! testGetMapping ";
+    }
+
+    @GetMapping("/{id}")
+    public String testControllerWithpathVaribales(@PathVariable(required = false)int id){
+
+        return "Hello World ID : " + id;
+    }
+
+    @GetMapping("/testRequestParam")
+    public String testRequestParam(@RequestParam(required = false) int id){
+
+        return "Hello World ID : " + id;
+    }
+
+    @GetMapping("/testRequestBody")
+    public String testRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO){
+        return "Hello World ID: " + testRequestBodyDTO.getId() + " Message : " + testRequestBodyDTO.getMessage();
+    }
+
+    @GetMapping("/testResponseBody")
+    public ResponseDTO<String> testControllerResponseBody() {
+        List<String> list = new ArrayList<>();
+        list.add("Hello World! I'm ResponseDTO");
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        return response;
+    }
+
+    @GetMapping("/testResponseEntity")
+    public ResponseEntity<?> testControllerResponseEntity() {
+        List<String> list = new ArrayList<>();
+        list.add("Hello World! I'm ResponseEntity. And you got 400!");
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        // http status를 400로 설정.
+        return ResponseEntity.ok().body(response);
     }
 }
