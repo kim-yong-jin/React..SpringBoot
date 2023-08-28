66f855835831ef92aefcd6e60ffb46e76fb65f54
yongjin <korea9053@gmail.com>
2023-08-28 22:14:47 +0900

    JWT 토큰 및 스프링 시큐리티 설정

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -6,6 +6,7 @@ import com.example.demo.model.TodoEntity;
 import com.example.demo.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -31,74 +32,61 @@ public class TodoController {
     }
 
     @PostMapping
-    public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto){
-        try {
-            String temporaryUserId = "temporary-user";
+    public ResponseEntity<?> createTodo(@AuthenticationPrincipal String userId, @RequestBody TodoDTO dto){
+      try {
+          TodoEntity entity = TodoDTO.toEntity(dto);
 
-            TodoEntity entity = TodoDTO.toEntity(dto);
-            entity.setId(null);
-            entity.setUserId(temporaryUserId);
+          entity.setId(null);
 
-            List<TodoEntity> entities = service.create(entity);
+          entity.setUserId(userId);
+          List<TodoEntity> entities = service.create(entity);
 
-            List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+          List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+          ResponseDTO<TodoDTO> response  = ResponseDTO.<TodoDTO>builder().data(dtos).build();
 
-            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
-
-            return ResponseEntity.ok().body(response);
-        } catch (Exception e){
+          return ResponseEntity.ok().body(response);
+      }catch (Exception e){
           String error = e.getMessage();
+
           ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+
           return ResponseEntity.badRequest().body(response);
-        }
+      }
     }
 
     @GetMapping
-    public ResponseEntity<?> retrieveTodoList(){
-        String temporaryUserId = "temporary-user";
-        List<TodoEntity> entities = service.retrieve(temporaryUserId);
+    public ResponseEntity<?> retrieveTodoList(@AuthenticationPrincipal String userId){
+        List<TodoEntity> entities = service.retrieve(userId);
 
         List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
 
         ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
 
         return ResponseEntity.ok().body(response);
-
     }
 
     @PutMapping
-    public ResponseEntity<?> updateTodo(@RequestBody TodoDTO dto){
-        String temporaryUserId = "temporary-user";
-
+    public ResponseEntity<?> updateTodo(@AuthenticationPrincipal String userId, @RequestBody TodoDTO dto){
         TodoEntity entity = TodoDTO.toEntity(dto);
-
-        entity.setUserId(temporaryUserId);
+        entity.setUserId(userId);
 
         List<TodoEntity> entities = service.update(entity);
 
         List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+        ResponseDTO<TodoDTO> reponse = ResponseDTO.<TodoDTO>builder().data(dtos).build();
 
-        ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
-
-        return ResponseEntity.ok().body(response);
-
+        return ResponseEntity.ok().body(reponse);
     }
 
     @DeleteMapping
-    public ResponseEntity<?> deleteTodo(@RequestBody TodoDTO dto) {
+    public ResponseEntity<?> deleteTodo(@AuthenticationPrincipal String userId,@RequestBody TodoDTO dto) {
         try {
-            String temporaryUserId = "temporary-user";
-
             TodoEntity entity = TodoDTO.toEntity(dto);
-
-            entity.setUserId(temporaryUserId);
+            entity.setUserId(userId);
 
             List<TodoEntity> entities = service.delete(entity);
-
             List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
-
             ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
-
             return ResponseEntity.ok().body(response);
         } catch (Exception e) {
             String error = e.getMessage();
--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,10 +3,13 @@ package com.example.demo.controller;
 import com.example.demo.dto.ResponseDTO;
 import com.example.demo.dto.UserDTO;
 import com.example.demo.model.UserEntity;
+import com.example.demo.security.TokenProvider;
 import com.example.demo.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +23,10 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private TokenProvider tokenProvider;
+
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO){
         try {
@@ -29,7 +36,7 @@ public class UserController {
 
             UserEntity user = UserEntity.builder()
                     .username(userDTO.getUsername())
-                    .password(userDTO.getPassword())
+                    .password(passwordEncoder.encode(userDTO.getPassword()))
                     .build();
 
             UserEntity registeredUser = userService.create(user);
@@ -50,14 +57,16 @@ public class UserController {
     public ResponseEntity<?> authenticate(@RequestBody UserDTO userDTO){
         UserEntity user = userService.getByCredentials(
             userDTO.getUsername(),
-            userDTO.getPassword());
+            userDTO.getPassword(),passwordEncoder);
 
         if(user != null){
-            final UserDTO responseUserDTO = UserDTO.builder()
-                    .username(user.getUsername())
-                    .id(user.getId())
-                    .build();
-            return ResponseEntity.ok().body(responseUserDTO);
+           final String token = tokenProvider.create(user);
+           final UserDTO responseUserDTO = UserDTO.builder()
+                   .username(user.getUsername())
+                   .id(user.getId())
+                   .token(token)
+                   .build();
+           return ResponseEntity.ok().body(responseUserDTO);
         } else {
             ResponseDTO responseDTO = ResponseDTO.builder()
                     .error("Login failed")
